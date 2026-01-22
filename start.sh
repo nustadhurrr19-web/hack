74cca20225c988987d465a1e2c648d444a62c388
nustadhurrr19-web <nustadhurrr19@gmail.com>
2026-01-22 19:13:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# 1. Start the brain in the background
 python fetcher.py &
-
-# 2. Start the web server with a LONG timeout (600 seconds)
-# We use 1 worker and 4 threads to handle the user + background tasks smoothly
 gunicorn server:app --bind 0.0.0.0:$PORT --timeout 600 --workers 1 --threads 4
